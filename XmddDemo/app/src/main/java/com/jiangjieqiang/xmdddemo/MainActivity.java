5b1f4d5ab015fa72b016b1458fc5afa9df435abd
jieqiang <1035288257jjq@gmail.com>
2016-07-23 21:38:34 +0800

    first commit

---
+++ XmddDemo/app/src/main/java/com/jiangjieqiang/xmdddemo/MainActivity.java
@@ -0,0 +1,85 @@
+package com.jiangjieqiang.xmdddemo;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Bind(R.id.recycler_main)
+    RecyclerView recyclerView;
+    private List<String> mDatas;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        initData();
+        initRecycler();
+    }
+
+    private void initData() {
+        mDatas = new ArrayList<String>();
+        for (int i = 'A'; i < 'z'; i++)
+        {
+            mDatas.add("" + (char) i);
+        }
+    }
+
+    private void initRecycler() {
+
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setAdapter(new MyAdapter());
+
+    }
+
+    class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder>
+    {
+
+        @Override
+        public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType)
+        {
+            MyViewHolder holder = new MyViewHolder(LayoutInflater.from(
+                    MainActivity.this).inflate(R.layout.layout_item, parent,
+                    false));
+            return holder;
+        }
+
+        @Override
+        public void onBindViewHolder(MyViewHolder holder, int position)
+        {
+            holder.tv.setText(mDatas.get(position));
+        }
+
+        @Override
+        public int getItemCount()
+        {
+            return mDatas.size();
+        }
+
+        class MyViewHolder extends RecyclerView.ViewHolder
+        {
+
+            TextView tv;
+
+            public MyViewHolder(View view)
+            {
+                super(view);
+                tv = (TextView) view.findViewById(R.id.text_num);
+            }
+        }
+    }
+
+}
---
+++ XmddDemo/app/src/main/java/com/jiangjieqiang/xmdddemo/util/FlingBehavior.java
@@ -0,0 +1,49 @@
+package com.jiangjieqiang.xmdddemo.util;
+
+import android.content.Context;
+import android.support.design.widget.AppBarLayout;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.v7.widget.RecyclerView;
+import android.util.AttributeSet;
+import android.view.View;
+
+
+/**
+ * Created by jiangjieqiang on 16/3/23.
+ */
+public final class FlingBehavior extends AppBarLayout.Behavior {
+    private static final int TOP_CHILD_FLING_THRESHOLD = 3;
+    private boolean isPositive;
+
+    public FlingBehavior() {
+    }
+
+    public FlingBehavior(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override
+    public boolean onStartNestedScroll(CoordinatorLayout parent, AppBarLayout child, View directTargetChild, View target, int nestedScrollAxes) {
+        return super.onStartNestedScroll(parent, child, directTargetChild, target, nestedScrollAxes);
+    }
+
+    @Override
+    public boolean onNestedFling(CoordinatorLayout coordinatorLayout, AppBarLayout child, View target, float velocityX, float velocityY, boolean consumed) {
+        if (velocityY > 0 && !isPositive || velocityY < 0 && isPositive) {
+            velocityY = velocityY * -1;
+        }
+        if (target instanceof RecyclerView && velocityY < 0) {
+            final RecyclerView recyclerView = (RecyclerView) target;
+            final View firstChild = recyclerView.getChildAt(0);
+            final int childAdapterPosition = recyclerView.getChildAdapterPosition(firstChild);
+            consumed = childAdapterPosition > TOP_CHILD_FLING_THRESHOLD;
+        }
+        return super.onNestedFling(coordinatorLayout, child, target, velocityX, velocityY, consumed);
+    }
+
+    @Override
+    public void onNestedPreScroll(CoordinatorLayout coordinatorLayout, AppBarLayout child, View target, int dx, int dy, int[] consumed) {
+        super.onNestedPreScroll(coordinatorLayout, child, target, dx, dy, consumed);
+        isPositive = dy > 0;
+    }
+}
